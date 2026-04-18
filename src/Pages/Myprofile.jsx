e53ae961a7e8b9a977827a7a4a9bb89f62b36c85
Moeez ali <moeez7697@gmail.com>
2026-04-18 21:38:41 +0500

    Backend: core priciples

--- src/Pages/Myprofile.jsx
+++ src/Pages/Myprofile.jsx
@@ -2,11 +2,57 @@ import React from 'react'
 import PatientDash from '../Components/PatientDash'
 import { useState, useEffect } from 'react'
 import Frame from "../assets/Frame.png"
-
+import axios from 'axios'
 const Myprofile = () => {
-
     const [file, setFile] = useState(null);
     const [url, setUrl] = useState(null);
+    const [files, setfile] = useState(null)
+    const [usern, setUserName] = useState("")
+    const [guardian, setguardian] = useState("")
+    const [email, setemail] = useState("")
+    const [cnic, setcnic] = useState("")
+    const [dob, setdob] = useState("")
+    const [address, setadress] = useState("")
+    const [phone, setphone] = useState("")
+    const [ephone, setephone] = useState("")
+    const [gender, setgender] = useState("")
+    const [blood, setblood] = useState("")
+    const [text, settext] = useState("")
+
+    const formsubmit = () => {
+        try {
+            const formdata = new FormData();
+            formdata.append("image", files);
+            formdata.append("usern", usern);
+            formdata.append("guardian", guardian);
+            formdata.append("email", email);
+            formdata.append("cnic", cnic);
+            formdata.append("dob", dob);
+            formdata.append("address", address);
+            formdata.append("phone", phone);
+            formdata.append("ephone", ephone);
+            formdata.append("gender", gender);
+            formdata.append("blood", blood);
+            formdata.append("text", text);
+            
+            console.log(typeof formdata)
+            axios.post(import.meta.env.VITE_API_URL + '/api/profile_edit', 
+                formdata,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data'
+                    }
+                }
+            ).then(res => console.log(res.data))
+        }
+        catch (error) {
+            console.log(error)
+            if (error.response.status === 400) {
+                alert(error.response.data.error)
+            }
+        }
+    }
+
 
     const handlechange = (e) => {
         setFile(e.target.files[0]);
@@ -27,10 +73,15 @@ const Myprofile = () => {
         <div>
             <PatientDash />
             <section className='edit-profile'>
-                <form action="">
+                <form onSubmit={() => formsubmit()}>
                     <label className='camera'>
                         <input type="file" placeholder='Profile picture' hidden
-                            onChange={handlechange}
+                            onChange={
+                                (e) => {
+                                    handlechange(e)
+                                    setfile(e.target.files[0])
+                                }
+                            }
                         />
                         {
                             url ?
@@ -46,27 +97,50 @@ const Myprofile = () => {
 
                     <div className='allign'>
                         <label htmlFor=""><i class="fa-regular fa-user"></i>Full Name</label>
-                        <input type="text" placeholder='Name' /> <br />
+                        <input type="text" placeholder='Name'
+                            onChange={(e) => setUserName(e.target.value)}
+                            required
+                        /> <br />
                         <label htmlFor=""><i class="fa-solid fa-people-group"></i>Guardian</label>
-                        <input type="text" placeholder='Guardian' /><br />
+                        <input type="text" placeholder='Guardian'
+                            required
+                            onChange={(e) => setguardian(e.target.value)}
+                        /><br />
                         <label htmlFor=""><i class="fa-solid fa-at"></i>Email</label>
-                        <input type="email" placeholder='Email' /> <br />
+                        <input type="email" placeholder='Email'
+                            required
+                            onChange={(e) => setemail(e.target.value)}
+                        /> <br />
                         <label htmlFor=""><i class="fa-regular fa-id-badge"></i>CNIC</label>
-                        <input type="number" placeholder='12345-1234567-8'/><br />
+                        <input type="number" placeholder='12345-1234567-8'
+                            required
+                            onChange={(e) => setcnic(e.target.value)}
+                        /><br />
                         <label htmlFor=""><i class="fa-regular fa-calendar"></i>DOB</label>
-                        <input type="date" placeholder='Date of Birth' /> <br />
+                        <input type="date" placeholder='Date of Birth'
+                            required
+                            onChange={(e) => setdob(e.target.value)}
+                        /> <br />
                     </div>
                     <div className='allign'>
                         <label htmlFor=""><i class="fa-solid fa-location-arrow"></i>Address</label>
-                         <input type="text" placeholder='Address' /> <br />
+                        <input type="text" placeholder='Address'
+                            required
+                            onChange={(e) => setadress(e.target.value)}
+                        /> <br />
                         <label htmlFor=""><i class="fa-solid fa-phone"></i>Phone Number</label>
-                        <input type="tel" placeholder='+92XXX-XXXXXXX' /> <br />
+                        <input type="tel" placeholder='+92XXX-XXXXXXX'
+                            required
+                            onChange={(e) => setphone(e.target.value)}
+                        /> <br />
                         <label htmlFor=""><i class="fa-solid fa-phone"></i>Emergency Contact</label>
-                        <input type="tel" placeholder='Emergency Contact' /> <br />
+                        <input type="tel" placeholder='Emergency Contact'
+                            required
+                            onChange={(e) => setephone(e.target.value)} /> <br />
 
                         <div className='label'>
                             <label htmlFor=""><i class="fa-solid fa-person-half-dress"></i>Gender</label> <br />
-                            <select name="" id="">
+                            <select name="" id="" onChange={(e) => setgender(e.target.value)} required>
                                 <option value="Male">Male</option>
                                 <option value="Female">Female</option>
                                 <option value="Others">Others</option>
@@ -74,7 +148,7 @@ const Myprofile = () => {
                         </div>
                         <div className='label'>
                             <label htmlFor=""><i class="fa-solid fa-droplet"></i>Blood Group</label> <br />
-                            <select name="Blood" id="Blood">
+                            <select name="Blood" id="Blood" onChange={(e) => setblood(e.target.value)} required>
                                 <option value="A+">A+</option>
                                 <option value="A-">A−</option>
                                 <option value="B+">B+</option>
@@ -87,7 +161,9 @@ const Myprofile = () => {
                         </div>
                     </div>
                     <div className='allign textarea'>
-                        <textarea placeholder='Medical History (Optional)' ></textarea>
+                        <textarea placeholder='Medical History (Optional)'
+                            onChange={(e) => settext(e.target.value)}
+                        ></textarea>
                         <button type='Submit'>Save</button>
                     </div>
                 </form>
